8d3aa0501562b1438ad2988e27082f870d8341da
Sharduli Pandey <shardulipandey8090@gmail.com>
2023-09-02 23:22:46 +0530

    fixed

--- src/components/ThemeChanger.jsx
+++ src/components/ThemeChanger.jsx
@@ -1,7 +1,7 @@
 import "../index.css";
 import ToggleButton from "./ToggleButton";
 import { useSelector } from "react-redux";
-import { lightTheme, darkTheme } from "../styles/theme.js";
+import { lightTheme, darkTheme } from "../styles/theme";
 
 const ThemeChanger = () => {
   const theme = useSelector((state) => state.theme.theme);
